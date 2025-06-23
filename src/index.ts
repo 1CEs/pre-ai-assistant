2a74da0128ec31311a31187989a52d816f4a67a6
1CEs <isongwut.me@gmail.com>
2025-06-23 07:37:02 +0700

    chore: implemented /assistant endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
-import { Elysia } from "elysia";
+import cors from "@elysiajs/cors";
+import { Elysia, t } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia({ prefix: "/api" })
+  .use(cors({ origin: "*" }))
+  .get("/assistant", () => "Hello, Elysia!", {
+    body: t.Object({
+      question: t.String({ description: "The question to ask the assistant" }),
+      answer: t.String({ description: "The answer from the assistant" }),
+      student_answer: t.String({
+        description: "The student's answer to the question",
+      }),
+    }),
+  })
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
